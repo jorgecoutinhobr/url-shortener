52bf78fbc91a8e68046c95e1db0d2ed21df4a5a4
Jorge Coutinho <jorgecsn@id.uff.br>
2024-03-20 17:34:38 -0300

    Feat: Add decode method and tests

--- app/models/short_code.rb
+++ app/models/short_code.rb
@@ -12,9 +12,19 @@ class ShortCode
       result.prepend char
       number = number / BASE
     end
+    
     result
   end
 
   def self.decode(string)
+    number = 0
+
+    string.reverse.each_char.with_index do |char, index|
+      power = BASE**index # 62^0, 62^1, 62^2, ...
+      index = ALPHABET.index(char)
+      number += index * power
+    end
+
+    number
   end
 end
